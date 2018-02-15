5e413e637dac78cc4e72a47378a2198d04e5d2c1
Rafael Poveda <raerpo@gmail.com>
2018-02-15 17:38:52 -0300

    extract cities

---
+++ cities.js
@@ -0,0 +1,55 @@
+module.exports = {
+  // Chile
+  osorno: 'ZOS',
+  concepcion: 'CCP',
+  iquique: 'IQQ',
+  antofagasta: 'ANF',
+  arica: 'ARI',
+  coquimbo: 'COW',
+  castro: 'WCA',
+  temuco: 'ZCO',
+  'isla de pascua': 'IPC',
+  valdivia: 'ZAL',
+  'la serena': 'lsc',
+  serena: 'lsc',
+  calama: 'cjc',
+  'punta arenas': 'PUQ',
+  'puerto aysen': 'WPA',
+  balmaceda: 'BBA',
+  'puerto montt': 'PMC',
+  // Colombia
+  bogota: 'BOG',
+  medellin: 'EOH',
+  'cartagena de indias': 'ctg',
+  'san andres': 'ADZ',
+  // Argentina
+  'buenos aires': 'BUE',
+  mendoza: 'MDZ',
+  // Peru
+  lima: 'lim',
+  // Brasil
+  'rio de janeiro': 'rio',
+  'sao paulo': 'sao',
+  // Uruguay
+  montevideo: 'mvd',
+
+  // Norte america
+  'new york': 'NYC',
+  miami: 'mia',
+  'san francisco': 'sfo',
+  'las vegas': 'las',
+  mexico: 'mex',
+  montreal: 'ymq',
+
+  // Europa
+  madrid: 'mad',
+  roma: 'rom',
+  paris: 'par',
+  berlin: 'ber',
+  moscu: 'mow',
+  barcelona: 'bcn',
+  // Asia
+  tokyo: 'tyo',
+  beijing: 'bjs',
+  'kuala lumpur': 'kul'
+}
--- index.js
+++ index.js
@@ -1,62 +1,7 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
-
-// I want to keep the city list small. If you want to include more cities you can do it here:
-const cityCodes = {
-  // Chile
-  osorno: 'ZOS',
-  concepcion: 'CCP',
-  iquique: 'IQQ',
-  antofagasta: 'ANF',
-  arica: 'ARI',
-  coquimbo: 'COW',
-  castro: 'WCA',
-  temuco: 'ZCO',
-  'isla de pascua': 'IPC',
-  valdivia: 'ZAL',
-  'la serena': 'lsc',
-  serena: 'lsc',
-  calama: 'cjc',
-  'punta arenas': 'PUQ',
-  'puerto aysen': 'WPA',
-  balmaceda: 'BBA',
-  'puerto montt': 'PMC',
-  // Colombia
-  bogota: 'BOG',
-  medellin: 'EOH',
-  'cartagena de indias': 'ctg',
-  'san andres': 'ADZ',
-  // Argentina
-  'buenos aires': 'BUE',
-  mendoza: 'MDZ',
-  // Peru
-  lima: 'lim',
-  // Brasil
-  'rio de janeiro': 'rio',
-  'sao paulo': 'sao',
-  // Uruguay
-  montevideo: 'mvd',
-
-  // Norte america
-  'new york': 'NYC',
-  miami: 'mia',
-  'san francisco': 'sfo',
-  'las vegas': 'las',
-  mexico: 'mex',
-  montreal: 'ymq',
-
-  // Europa
-  madrid: 'mad',
-  roma: 'rom',
-  paris: 'par',
-  berlin: 'ber',
-  moscu: 'mow',
-  barcelona: 'bcn',
-  // Asia
-  tokyo: 'tyo',
-  beijing: 'bjs'
-}
+const cityCodes = require('./cities');
 
 app.get('/', (req, res) => {
   res.send('Huemul Airlines...');
